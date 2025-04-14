2a7970cc7612111440795a73eacabcbf6f052f3a
MichelaMilzoni <michelamilzoni0@gmail.com>
2025-04-14 16:46:41 +0200

    snack 1:  array di oggetti

--- snack 1/main.js
+++ snack 1/main.js
@@ -7,7 +7,28 @@
 //* Creo un array di oggetti, che contiene due proprietà:
 //* nome: stringa per nome bici
 //* peso: numero per peso bici
-
+const bicycles = [
+    {
+        nameBicycle: `Specialized`,
+        bikeWeight: 9.5
+    },
+    {
+        nameBicycle: `BMC`,
+        bikeWeight: 7.2
+    },
+    {
+        nameBicycle: `Scott`,
+        bikeWeight: 6.8
+    },
+    {
+        nameBicycle: `Colnago`,
+        bikeWeight: 7
+    },
+    {
+        nameBicycle: `Cannondale`,
+        bikeWeight: 11.2
+    }
+]
 
 // TODO 2:
 //* confronto il peso di ogni bici per trovare quella con il peso minore
