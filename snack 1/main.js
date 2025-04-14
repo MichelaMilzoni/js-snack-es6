9933f01c8e1b2ee5f859c0cd6dcfd1ff9c5b2878
MichelaMilzoni <michelamilzoni0@gmail.com>
2025-04-14 16:36:49 +0200

    snack 1: passaggi

--- snack 1/main.js
+++ snack 1/main.js
@@ -2,3 +2,14 @@
 // ? Creare un array di oggetti:
 // ? Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // ? Stampare a schermo la bici con peso minore.
+
+// TODO 1: 
+//* Creo un array di oggetti, che contiene due proprietà:
+//* nome: stringa per nome bici
+//* peso: numero per peso bici
+
+
+// TODO 2:
+//* confronto il peso di ogni bici per trovare quella con il peso minore
+// TODO 3:
+//* mostro la bici più leggera
