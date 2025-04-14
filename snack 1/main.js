0cf57d0ea6dd10d321770b62b8bc5407eaae7b56
MichelaMilzoni <michelamilzoni0@gmail.com>
2025-04-14 16:59:28 +0200

    snack 1: ciclo for per confronto peso e stampa

--- snack 1/main.js
+++ snack 1/main.js
@@ -32,5 +32,18 @@ const bicycles = [
 
 // TODO 2:
 //* confronto il peso di ogni bici per trovare quella con il peso minore
+let lightBicycle = bicycles[0]; //creo una variabile dandogli il valore del primo indici del mio array, supponendo che sia la più leggera
+
+// ciclo for per confrontare il primo indice bicycles[0] con il successivo
+// il mio confronto parte quindi dal primo indice i = 1, scorre per tutta la lunghezza dell'array e a ogni giro incrementa di 1 
+for (let i = 1; i < bicycles.length; i++){
+    // se il peso dell'indice che sto controllando bicycles[i].bikeWeight è minore lightBicycle.bikeWeight (variabile)
+    if (bicycles[i].bikeWeight < lightBicycle.bikeWeight) {
+        // allora la variabile assume il valore dell'indice che sto controllando
+        lightBicycle = bicycles[i];
+    }
+}
+
 // TODO 3:
 //* mostro la bici più leggera
+console.log(`La bici più leggera è: ${lightBicycle.nameBicycle} con un peso di ${lightBicycle.bikeWeight}`)
